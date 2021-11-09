417d3b90d077f375ba4bbb837a65e9e8b37cebf1
Evan West <evtwest@yahoo.com>
2021-11-08 20:12:52 -0500

    num_nodes in buffer_expr

--- buffer_expr/buffer_expr.cpp
+++ buffer_expr/buffer_expr.cpp
@@ -14,13 +14,13 @@ int main(int argc, char** argv) {
   if (argc != 4) {
     std::cout << "Incorrect number of arguments. "
                  "Expected three but got " << argc-1 << std::endl;
-    std::cout << "Arguments are: input_stream, output_file, updates_in_sketch" << std::endl;
+    std::cout << "Arguments are: input_stream, output_file, num_nodes" << std::endl;
     exit(EXIT_FAILURE);
   }
 
   std::string input  = argv[1];
   std::string output = argv[2];
-  int updates_in_sketch = std::stoi(argv[3]); // number of bytes it takes to represent a sketch
+  int num_nodes = std::stoi(argv[3]); // number of bytes it takes to represent a sketch
   if (max_thr <= 0 || max_thr > (1 << 20)) {
     printf("Argument sketch_bytes out of range [1,1MiB]");
     exit(EXIT_FAILURE);
@@ -29,7 +29,9 @@ int main(int argc, char** argv) {
   // backup the current configuration
   backup_configuration();
 
-  // TODO: Construct the sizes
+  // Calculate the number of updates per sketch sized buffer
+  int updates_in_sketch = 42 * pow(log2(num_nodes), 2) / (log2(3) - 1); 
+
   std::vector<int> sizes{2, 1, -2, -3, -4, -6, -10};
 
   // assert that we also check 1000, 500, 250, and 1 updates per buffer
