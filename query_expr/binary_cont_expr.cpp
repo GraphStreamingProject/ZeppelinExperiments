83a1b9b25d347fee37ba8cb3ebea4fd9efa1dc35
Victor Zhang <vcz4@scarletmail.rutgers.edu>
2022-01-25 22:23:49 -0500

    fix: binary cont experiment

--- query_expr/binary_cont_expr.cpp
+++ query_expr/binary_cont_expr.cpp
@@ -1,4 +1,3 @@
-#include <fstream>
 #include <unordered_map>
 #include <iostream>
 
@@ -12,12 +11,10 @@ void test_continuous(std::string input_file, unsigned samples) {
   BinaryGraphStream stream {input_file, 32*1024};
   node_id_t n = stream.nodes();
   uint64_t  m = stream.edges();
+  size_t updates_per_sample = m / samples;
 
   Graph g{n};
 
-  size_t total_edges = static_cast<size_t>(n - 1) * n / 2;
-  size_t updates_per_sample = m / samples;
-//  std::vector<bool> adj(total_edges);
   unsigned long num_failure = 0;
   std::vector<double> flush_times (samples, 0.0);
   std::vector<double> cc_times (samples, 0.0);
@@ -34,12 +31,11 @@ void test_continuous(std::string input_file, unsigned samples) {
             << empty_time << "\nFLush time: " << empty_flush << "\nCC time: "
             << empty_cc << std::endl;
 
-  node_id_t t,a,b;
   for (unsigned long i = 0; i < samples; i++) {
     std::cout << "Starting updates" << std::endl;
     for (unsigned long j = 0; j < updates_per_sample; j++) {
       GraphUpdate upd = stream.get_edge();
-      g.update({{a,b},INSERT});
+      g.update(upd);
     }
     try {
       std::cout << "Running cc" << std::endl;
