9e8e8ea8345c0f255b2f7928be4ed3076d0dbec7
kennyzzhang <kennyzzhang@gmail.com>
2022-09-08 19:50:12 -0400

    Added dataset links

--- run.sh
+++ run.sh
@@ -5,14 +5,10 @@ function runcmd {
   "$@"
 }
 
-echo 'Running CMake build'
-
-# runcmd mkdir -p build
-# runcmd cd build
-# runcmd cmake ..
-# runcmd cmake --build .
-
-echo 'Finished running CMake build'
+echo 'Specify parameters for experiments'
+# TODO: cont_test: samples, runs
+# TODO: parallel_experiment: max_threads
+# TODO: query_test: num_buffer_elems
 
 echo 'Select datasets to download'
 
@@ -27,16 +23,38 @@ datasets=(
   'google-plus'
   'web-uk'
 )
-datalinks=(
-  'http://whereverkron13'
-  'http://whereverkron15'
-  'http://whereverkron16'
-  'http://whereverkron17'
-  'http://whereverkron18'
-  'http://whereverp2p-guntella'
-  'http://whereverrec-amazon'
-  'http://wherevergoogle-plus'
-  'http://whereverweb-uk'
+dataset_sizes=(
+  '150.6MB'
+  '2.3GB'
+  '9.4GB'
+  '37.5GB'
+  '150.0GB'
+  '2.5MB'
+  '2.1MB'
+  '232.9MB'
+  '200.1MB'
+)
+dataset_filenames=(
+  'kron_13_stream_binary'
+  'kron_15_stream_binary'
+  'kron_16_stream_binary'
+  'kron_17_stream_binary'
+  'kron_18_stream_binary'
+  'p2p_gnutella_stream_binary'
+  'rec_amazon_stream_binary'
+  'google_plus_stream_binary'
+  'web_uk_stream_binary'
+)
+dataset_links=(
+  'https://graphzeppelin-datasets.s3.amazonaws.com/kron_13_stream_binary'
+  'https://graphzeppelin-datasets.s3.amazonaws.com/kron_15_stream_binary'
+  'https://graphzeppelin-datasets.s3.amazonaws.com/kron_16_stream_binary'
+  'https://graphzeppelin-datasets.s3.amazonaws.com/kron_17_stream_binary'
+  'https://graphzeppelin-datasets.s3.amazonaws.com/kron_18_stream_binary'
+  'https://graphzeppelin-datasets.s3.amazonaws.com/p2p_gnutella_stream_binary'
+  'https://graphzeppelin-datasets.s3.amazonaws.com/rec_amazon_stream_binary'
+  'https://graphzeppelin-datasets.s3.amazonaws.com/google_plus_stream_binary'
+  'https://graphzeppelin-datasets.s3.amazonaws.com/web_uk_stream_binary'
 )
 declare -a selected
 while true; do
@@ -45,7 +63,7 @@ while true; do
   for ((i = 0; i < ${#datasets[@]}; i++)); do
     selstr="${selected[$i]:+*}"
     selstr="${selstr:- }"
-    echo "${selstr} $(($i + 1))) ${datasets[$i]}"
+    echo "${selstr} $(($i + 1))) ${datasets[$i]} ${dataset_sizes[$i]}"
   done
   echo 'Enter number to toggle selection, or "done" to continue'
 
@@ -82,26 +100,52 @@ while true; do
   done
 done
 
+echo 'Running CMake build'
+
+runcmd mkdir -p build
+runcmd cd build
+runcmd cmake ..
+runcmd cmake --build .
+
+echo 'Finished running CMake build'
+
 echo 'Downloading datasets'
 
 runcmd mkdir -p datasets
 for ((i = 0; i < ${#datasets[@]}; i++)); do
   if [[ -n "${selected[$i]}" ]]; then
-    #TODO: actual download
-    echo "$i: ${datasets[$i]}: ${datalinks[$i]}"
+    runcmd wget -O "datasets/${dataset_filenames[$i]}" "${dataset_links[$i]}"
   fi
 done
 
 echo 'Finished downloading datasets'
 
+dataset_files=(datasets/*)
 echo 'Running experiments'
-for DATASET in datasets/*; do
-  runcmd ./buffersize_experiment
+runcmd mkdir -p buffer_expr_results
+for dataset in "${dataset_files[@]}"; do
+  runcmd ./buffersize_experiment "$dataset" "buffer_expr_results/${dataset##*/}.out"
 done
-runcmd ./cont_test
-runcmd ./parallel_experiment
-runcmd ./query_test
+
+runcmd mkdir -p cont_expr_results
+for dataset in "${dataset_files[@]}"; do
+  runcmd ./cont_test "$dataset" "TODO: samples" "TODO: runs"
+done
+
+runcmd mkdir -p parallel_expr_results
+for dataset in "${dataset_files[@]}"; do
+  runcmd ./parallel_experiment "$dataset" "parallel_expr_results/${dataset##/}.out" "TODO: threads"
+done
+
+runcmd mkdir -p query_expr_results
+for dataset in "${dataset_files[@]}"; do
+  runcmd ./query_test "$dataset" "TODO: num_buffer_elems"
+done
+
 runcmd ../sketch_expr/run_sketch_expr.sh
-runcmd ../speed_expr/run_experiments.sh
-runcmd ../speed_expr/run_unlim_experiments.sh
+
+runcmd ../speed_expr/run_experiments.sh "${dataset_files[@]}"
+
+runcmd ../speed_expr/run_unlim_experiments.sh "${dataset_files[@]}"
+
 echo 'Finished running experiments'
