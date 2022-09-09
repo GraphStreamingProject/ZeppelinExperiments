e4f944161c5229cc13ca7c74ed0979e49e262e25
Victor Zhang <xskt.victor@gmail.com>
2022-09-08 23:14:50 -0700

    fix: remove group size expr

--- parallel_expr/parallel_expr.cpp
+++ parallel_expr/parallel_expr.cpp
@@ -27,23 +27,6 @@ int main(int argc, char** argv) {
     printf("Argument CPU cores invalid. Out of range [1,100");
     exit(EXIT_FAILURE);
   }
-  // max_thr for group size experiment needs to be divisible by 20
-  int gr_max_thr = max_thr - (max_thr % 20);
-
-  std::vector<int> sizes{1,2,4,10,20};
-
-  // Group Size Experiment
-  printf("Running group_size experiment\n");
-  for (int size : sizes) {
-    if (size > gr_max_thr) break;
-
-    // for this experiment we set group_size and num_groups
-    sys_config conf;
-    conf.group_size = size;
-    conf.num_groups = gr_max_thr / size;
-    perform_insertions(input, output + "_group_exp_" + std::to_string(size), conf);
-  }
-
 
   // Num Threads Experiment
   printf("Running num_threads experiment\n");
