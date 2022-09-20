a3854826aba12ecba3ca7146c7558988764fe5bb
Evan West <etwest@cs.stonybrook.edu>
2022-09-19 22:42:44 -0400

    Update plot.sh

--- plotting/plot.sh
+++ plotting/plot.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
+
+mkdir latex/images 2> /dev/null
+
+# Create images
 cd R_scripts
 Rscript parallel_plot.R
 mv parallel.png ../latex/images/parallel.png
+
 Rscript parallel_plot3.R
 mv parallel3.png ../latex/images/parallel3.png
+
 Rscript query.R
 mv query.png ../latex/images/query.png
+
 Rscript query_disk.R
 mv query_disk.png ../latex/images/query_disk.png
+
 Rscript space_plot.R
 mv size.png ../latex/images/size.png
+
 Rscript speed_plot.R
 mv speed.png ../latex/images/speed.png
+
 Rscript speed_plot2.R
 mv speed_unlim.png ../latex/images/speed_unlim.png
 
+# Create pdf document with images and tables
 cd ../latex
 pdflatex -synctex=1 -shell-escape -interaction=nonstopmode main.tex
 mv main.pdf ../../figures.pdf
