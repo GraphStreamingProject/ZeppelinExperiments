772dafa883cc9697c9f831407c4ac4ee8c0bb705
Evan West <etwest@cs.stonybrook.edu>
2024-02-15 22:53:25 -0500

    half working fix for incorrect legend in plots

--- plotting/R_scripts/query_disk.R
+++ plotting/R_scripts/query_disk.R
@@ -7,8 +7,8 @@ ggplot() +
   geom_line(data = speed, mapping = aes(x = percent, y = flush+cc, color = system, linetype =system), size = 0.5) +
   labs(y=expression(paste("Query time (s)")), x ="percent of stream") +
   labs(color = "System\n") +
-  scale_color_hue(labels = c("Aspen", "GraphZeppelin"))+
-  scale_linetype_manual("", breaks=c("aspen_top", "stream_mem")
-                        , values=c("dashed", "solid"))
+  scale_color_hue(labels = c("GraphZeppelin", "Aspen"))+
+  scale_linetype_manual("", breaks=c("stream_mem", "aspen_top")
+                        , values=c("solid", "dashed"))
 
 ggsave(filename = "query_disk.png", width = 6, height = 2, unit = "in")
--- plotting/R_scripts/speed_plot2.R
+++ plotting/R_scripts/speed_plot2.R
@@ -10,8 +10,8 @@ ggplot() +
   labs(color = "System\n") +
   theme(legend.position = c(0.45, -0.30), legend.direction = 'horizontal', plot.margin = grid::unit(c(0,1,2,0), 'lines')) +
   guides(linetype="none") +
-  scale_color_hue(labels = c("Aspen", "GraphZeppelin Gutter Tree", "GraphZepplin leaf gutters")) + 
-  scale_linetype_manual("", breaks=c("aspen_top", "stream_mem", "stream_ext")
-                        , values=c("dashed", "longdash","solid"))
+  scale_color_hue(labels = c("GraphZeppelin Gutter Tree", "GraphZepplin leaf gutters", "Aspen")) + 
+  scale_linetype_manual("", breaks=c("stream_mem", "stream_ext", "aspen_top")
+                        , values=c("longdash", "solid", "dashed"))
 
 ggsave(filename = "speed_unlim.png", width = 6, height = 3, unit = "in")
